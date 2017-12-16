100aed7b70572b6a09b1771e0f26efed3eb7ddf9
kiraind <ne.eshte@otso.city>
2017-12-16 17:10:25 +0300

    updated buld scripts

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-node build/builder.js
+echo "Building..."
+node build/builder.js src/index.html Core/page.html
+echo "Uploading..."
+node node_modules/nodemcu-tool/bin/nodemcu-tool.js -p %1 -b 115200 upload Core/page.html

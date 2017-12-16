26fac98c2a17762fb3f3321f6feb2f3c086fae96
kiraind <ne.eshte@otso.city>
2017-12-16 17:12:47 +0300

    fix conflict

--- upload_build.bat
+++ upload_build.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Building...
-node build/builder.js src/index.html Core/page.html
+node "%~dp0build\builder.js" "%~dp0src\index.html" "%~dp0Core\page.html"
 echo Uploading...
 node node_modules/nodemcu-tool/bin/nodemcu-tool.js -p %1 -b 115200 upload Core/page.html

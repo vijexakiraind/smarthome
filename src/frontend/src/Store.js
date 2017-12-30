11a4371438eb06d0e4e42dc2d2bdab3a7d84e649
kiraind <ne.eshte@otso.city>
2017-12-30 05:20:30 +0300

    theme adjustments

--- src/frontend/src/Store.js
+++ src/frontend/src/Store.js
@@ -4,7 +4,7 @@ import * as actionCreators from './Actions/'
 import rootReducer from './Reducers/'
 
 const defaultState =  { 
-    darkTheme: true
+    darkTheme: false
 }
 
 const store = createStore(rootReducer, defaultState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())

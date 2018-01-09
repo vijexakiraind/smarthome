690660014b25d2946fb708e000a319eae43e4d4e
kiraind <ne.eshte@otso.city>
2018-01-09 23:51:11 +0300

    local ip setting logic

--- src/frontend/src/Components/IPSettings/IPSettings.js
+++ src/frontend/src/Components/IPSettings/IPSettings.js
@@ -8,15 +8,22 @@ export default class IPSettings extends React.Component {
 
         this.state = {
             globalValid: true,
-            localValid: true
+            localValid: true,
+            globalIp: this.props.serverGlobalIp,
+            localIp: this.props.serverLocalIp
         }
 
         this.validateLocal = this.validateLocal.bind(this)
         this.validateGlobal = this.validateGlobal.bind(this)
+        this.trySaveLocal = this.trySaveLocal.bind(this)
     }
 
     validateLocal() {
         const ip = this.refs.local.value
+
+        this.setState({
+            localIp: ip
+        })
 
         if(!/^([0-9]{1,3}\.){3}[0-9]{1,3}$/.test(ip) || 
             ip.split('.').some(n => parseInt(n, 10) > 255)) {
@@ -30,6 +37,10 @@ export default class IPSettings extends React.Component {
     validateGlobal() {
         const ip = this.refs.global.value
 
+        this.setState({
+            globalIp: ip
+        })
+
         if(!/^([0-9]{1,3}\.){3}[0-9]{1,3}$/.test(ip) || 
             ip.split('.').some(n => parseInt(n, 10) > 255)) {
             this.setState({ globalValid: false })
@@ -37,6 +48,16 @@ export default class IPSettings extends React.Component {
         }
 
         this.setState({ globalValid: true })
+    }
+
+    trySaveLocal() {
+        if(this.state.localValid)
+            this.props.setServerLocalIp(this.state.localIp)
+        else
+            this.setState({
+                localIp: this.props.serverLocalIp,
+                localValid: true
+            })
     }
 
     render() {
@@ -52,7 +73,7 @@ export default class IPSettings extends React.Component {
                         className={'IPSettings-input' + (!this.state.globalValid ? ' error' : '')}
                         type="text"
                         placeholder="12.123.12.123"
-                    />
+                    />  {/* todo global ip logic */}
                 </div>
                 <div className="IPSettings-item">
                     <div className="IPSettings-label">
@@ -64,6 +85,8 @@ export default class IPSettings extends React.Component {
                         className={'IPSettings-input' + (!this.state.localValid ? ' error' : '')}
                         type="text"
                         placeholder="192.168.1.123"
+                        value={this.state.localIp}
+                        onBlur={this.trySaveLocal}
                     />
                 </div>
             </div>
